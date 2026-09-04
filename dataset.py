9cc2cd194f6ff07b20c2ce02d5ccf46bd1044483
Bia Marques <santosmarquesbeatriz@gmail.com>
2026-09-04 17:21:24 -0300

    Substitui o cv2 por PIL.Image; adicionei a instance_mask, a máscara de instância onde cada célula recebe um id, que é retornada junto com a mask e a image

--- dataset.py
+++ dataset.py
@@ -1,7 +1,7 @@
 import os
 from typing import Any
 import numpy as np
-import cv2
+from PIL import Image
 import torch
 from torch.utils.data import Dataset
 
@@ -21,26 +21,29 @@ class DSB2018Dataset(Dataset):
         img_folder = os.path.join(self.root_dir, img_id, 'images')
         mask_folder = os.path.join(self.root_dir, img_id, 'masks')
 
+        # 1. Carregar imagem original usando PIL
         img_path = os.path.join(img_folder, img_id + '.png')
-        image = cv2.imread(img_path, cv2.IMREAD_COLOR)
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        image = cv2.resize(image, self.image_size)
+        image = Image.open(img_path).convert('RGB')
+        image = image.resize(self.image_size)
+        image = np.array(image, dtype=np.float32)
 
         mask = np.zeros(self.image_size, dtype=np.float32)
+        instance_mask = np.zeros(self.image_size, dtype=np.int32)
 
-        for mask_file in os.listdir(mask_folder):
+        # 2. Carregar máscaras individuais e gerar a máscara binária e a máscara de instâncias
+        for i, mask_file in enumerate(os.listdir(mask_folder), start=1):
             m_path = os.path.join(mask_folder, mask_file)
-            instance_mask = cv2.imread(m_path, cv2.IMREAD_GRAYSCALE)
-            instance_mask = cv2.resize(instance_mask, self.image_size, interpolation=cv2.INTER_NEAREST)
+            inst_mask = Image.open(m_path).convert('L')
+            inst_mask = inst_mask.resize(self.image_size, Image.NEAREST)
+            inst_np = np.array(inst_mask) > 0
 
-            mask = np.maximum(mask, instance_mask)
+            mask = np.maximum(mask, inst_np.astype(np.float32))
+            instance_mask[inst_np] = i
 
-        mask = (mask > 0).astype(np.float32)
         image = image.transpose((2, 0, 1)) / 255.0
 
         image = torch.tensor(image, dtype=torch.float32)
-
         mask = torch.tensor(mask, dtype=torch.float32).unsqueeze(0)
+        instance_mask = torch.tensor(instance_mask, dtype=torch.long)
 
-        return image, mask
-                
+        return image, mask, instance_mask
